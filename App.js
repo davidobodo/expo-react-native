cf6fe403e91b9ed72b188815ce9f491d9b8f41da
Obodo David <obododavid5@gmail.com>
2023-03-07 16:18:29 +0100

    commit

--- App.js
+++ App.js
@@ -1,20 +1,48 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Button, Pressable } from "react-native";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+
+			<View style={styles.bottom}>
+				<Text style={styles.subtitle}>New fashion collection</Text>
+				<Text style={styles.title}>Start discovering your unique fashion style</Text>
+
+				<Pressable style={styles.button}>
+					<Text style={styles.buttonText}>Get started</Text>
+				</Pressable>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: "white",
+		justifyContent: "flex-end",
+	},
+	bottom: {
+		backgroundColor: "red",
+		height: "33%",
+		padding: 20,
+	},
+	subtitle: {
+		marginBottom: 10,
+	},
+	title: {
+		fontSize: 24,
+		marginBottom: 20,
+	},
+	button: {
+		backgroundColor: "black",
+		alignItems: "center",
+		padding: 15,
+		borderRadius: 20,
+	},
+	buttonText: {
+		color: "#fff",
+	},
 });
